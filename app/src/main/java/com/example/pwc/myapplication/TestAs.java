aeb201bdc91baeca3cea1fc15a15dc4402c4ffbc
myAndroid <936237201@qq.com>
2015-12-23 18:58:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/pwc/myapplication/TestAs.java
@@ -0,0 +1,19 @@
+package com.example.pwc.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by PWC on 2015/12/2.
+ */
+public class TestAs extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testas);
+
+        findViewById(R.id.tv_asTest);
+
+    }
+}
